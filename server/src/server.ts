469d60b2545fea76aca2c9e5d76c6f486f91c572
chnds <caio.hnascimento98@gmail.com>
2022-02-08 22:51:54 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+
+const app = express();
+
+const users = [
+    'Caio',
+    'Diego',
+    'Laura',
+    'Paulo',
+];
+
+app.get('/users',(request, response) => {
+
+   return response.json(users);
+});
+
+app.get('/users/:id', (request,response)=> {
+    const id = number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users',(request,response) => {
+    const user = {
+        name: 'Diego',
+        email: 'Diego@rocketseat.com.br',
+    }
+
+    return response.json(user);
+})
+
+app.listen(3333);
